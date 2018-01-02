7d32fd5218f937bd785d0e46f6b3ee82779978e6
苏祥凤 <suxiangfeng@suxiangfengdeMacBook-Pro-3.local>
2018-01-02 11:06:06 +0800

    add LightBox Component

---
+++ src/components/LightBox/Index.js
@@ -0,0 +1,115 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, Modal, Carousel, Icon } from 'antd';
+
+import styles from './Index.less';
+const SamplePrevArrow = (props) => {
+    const { className, style, onClick } = props
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: 'block', width: 25, height: 25, fontSize: 25 }}
+            onClick={onClick}
+        >
+            <Icon type="left" style={{ color: 'rgba(0, 0, 0, 0.45)' }} />
+        </div>
+    );
+}
+const SampleNextArrow = (props) => {
+    const { className, style, onClick } = props
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: 'block', width: 25, height: 25, fontSize: 25 }}
+            onClick={onClick}
+        >
+            <Icon type="right" style={{ color: 'rgba(0, 0, 0, 0.45)' }} />
+        </div>
+    );
+}
+// const 
+export default class Index extends PureComponent {
+    static propTypes = {
+        images: PropTypes.arrayOf(PropTypes.shape({
+            src: PropTypes.string.isRequired,
+        })).isRequired
+    }
+    static defaultProps = {
+        images: [{ src: 'https://images.unsplash.com/photo-1471127432458-65206be149c9?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' },
+        { src: 'https://images.unsplash.com/photo-1453550486481-aa4175b013ea?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' },
+        { src: 'https://images.unsplash.com/photo-1471127432458-65206be149c9?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' },
+        { src: 'https://images.unsplash.com/photo-1471127432458-65206be149c9?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' },
+        { src: 'https://images.unsplash.com/photo-1471127432458-65206be149c9?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' },
+        { src: 'https://images.unsplash.com/photo-1471127432458-65206be149c9?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' },
+        { src: 'https://images.unsplash.com/photo-1471127432458-65206be149c9?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' },
+        { src: 'https://images.unsplash.com/photo-1471127432458-65206be149c9?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' },
+        { src: 'https://images.unsplash.com/photo-1471127432458-65206be149c9?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159' }
+        ]
+    }
+    state = {
+        modalVisible: false,
+        currentIndex: 0
+    }
+
+    showModal = (index) => {
+        this.setState({
+            modalVisible: true,
+            currentIndex: index
+        });
+    }
+    hideModal = (currentIndex) => {
+        this.setState({
+            modalVisible: false,
+        });
+    }
+    render() {
+        const { images } = this.props;
+        const { modalVisible, currentIndex } = this.state;
+        const carouselProps = {
+            arrows: true,
+            adaptiveHeight: true,
+            initialSlide: currentIndex,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />
+        };
+        return (
+            <div className={styles.lightBoxWrap}>
+                <Row gutter={2}>
+                    {
+                        images.map((image, i) => {
+                            return (
+                                <Col key={`img_${i}`} md={3} sm={24} style={{ marginBottom: 16 }} >
+                                    <a className={styles.imgWrap} onClick={() => { this.showModal(i) }}>
+                                        <img src={image.src} />
+                                    </a>
+                                </Col>
+                            )
+                        })
+                    }
+                </Row>
+                <Modal
+                    title={null}
+                    // width={700}
+                    visible={modalVisible}
+                    footer={null}
+                    destroyOnClose={true}
+                    onCancel={() => this.hideModal()}
+                >
+                    <div style={{ padding: '16px 0 0' }}>
+                        <Carousel {...carouselProps}>
+                            {
+                                images.map((img, i) =>
+                                    <div key={i}>
+                                        <div className="image-modal-container">
+                                            <img src={img.src} />
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        </Carousel>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
+}
